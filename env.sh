09848db317e40bdc427aef962c40893e3ce68428
Motyak <m0tyak_@live.fr>
2026-01-31 09:41:46 +0100

    better reporting when test couldnt be handled

--- env.sh
+++ env.sh
@@ -7,8 +7,9 @@ function __doit {
     eval "
     function preprocess_prog {
         cd \"${SCRIPT_DIR}\"
-        mlp/mlp2.sh src/main.mlp -I src
+        mlp/mlp2.sh src/main.mlp -I src || exit_code=\$?
         cd - > /dev/null
+        return \${exit_code:-0}
     }
     "
     export -f preprocess_prog
--- tests/.test.sh
+++ tests/.test.sh
@@ -7,17 +7,24 @@ set -o errexit
 set -o nounset
 # set -o xtrace #debug
 
+trap err ERR
+function err {
+    exit_code=${1:-$?}
+    [ "$test" != "" ] && >&2 echo "Test $test couldn't be performed"
+    exit $exit_code
+}
+
 DRYRUN=${DRYRUN:-0}
 
 cd "$(dirname "$0")"
 test="$(basename "$0")"; test="${test%.sh}"
 source "${test}.args.sh" # get 'args' variable from individual test
 preprocess_prog; ((DRYRUN)) && exit 0
-prog_out="$("$PROG" "${args[@]}" < "${test}.in.txt"; echo -n x)"
+prog_out="$("$PROG" "${args[@]}" < "${test}.in.txt" && echo -n x)"
 >/dev/null diff "${test}.out.txt" <(printf "%s" "$prog_out" | head -n-1) || diffcode=$?
-[ ${diffcode:-0} -eq 2 ] && exit 2
+[ ${diffcode:-0} -eq 2 ] && err 2
 [ ${diffcode:-0} -eq 1 ] && {
     echo "=== ❌ $test ==="
-    git --no-pager diff --no-index "${test}.out.txt" <(printf "%s" "$prog_out" | head -n-1)
+    git --no-pager diff --no-index "${test}.out.txt" <(printf "%s" "$prog_out" | head -n-1) || exit 0
 }
 echo "=== ✅ $test ==="
